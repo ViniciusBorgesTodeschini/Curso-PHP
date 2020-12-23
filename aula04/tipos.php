9096f6718564105b80bdd09521568d9cadad410d
Vinicius Borges Todeschini <75043020+ViniciusBorgesTodeschini@users.noreply.github.com>
2020-12-23 18:31:32 -0300

    Create tipos.php

---
+++ aula04/tipos.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../_css/estilo.css" />
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <?php
+            $idade = 24;
+            $nome = "Vinícius";
+            echo "$nome tem $idade anos.";
+
+        ?>
+    </div>
+</body>
+</html>
