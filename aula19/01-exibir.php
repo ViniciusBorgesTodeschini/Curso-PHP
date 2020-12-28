e72cf06fa3cdcdbe0ef63f3e64c9549e75e4d6a5
Vinicius Borges Todeschini <75043020+ViniciusBorgesTodeschini@users.noreply.github.com>
2020-12-28 11:22:25 -0300

    update-aula19-FIM

---
+++ aula19/01-exibir.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../_css/estilo.css">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <pre>
+            <?php 
+                $v = array(1,9,7,9,4);
+                echo "O vetor tem " . count ($v). " elementos <br>";
+                print_r ($v);
+            ?>
+         </pre>
+    </div>
+</body>
+</html>
